928b7ab31f006d7accb7add3395fb70f23cb326a
Damian Duy <gamergray8@gmail.com>
2020-09-22 13:30:46 +0200

    Started rewritig bad code, added class Messages, edited massive while loop

--- Guessing_Game.cpp
+++ Guessing_Game.cpp
@@ -1,196 +1,198 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include <unistd.h>
+
+#include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include <unistd.h>
-#include <cmath>
-class guessingGame
-{
-    public:
+#include <fstream>
+#include <iostream>
+#include <string>
 
-    std::string chosenSymbol;
-    std::string userInput;
-    double money;
-    double bet;
+class Messages {
+ public:
+  void menu()  // Shows the menu
+  {
+    std::cout << std::endl;
+    std::cout << "0) Exit" << std::endl << "1) Play" << std::endl;
+    std::cout << std::endl;
+  }
+  void beginMessage() {
+    std::cout << "Welcome to the Guessing Game!" << std::endl;
+    sleep(2);
+    std::cout << "You'll bet money on one of the following symbols: "
+              << std::endl;
+    sleep(2);
+    std::cout << "-.-" << std::endl;
+    sleep(1);
+    std::cout << "@.@" << std::endl;
+    sleep(1);
+    std::cout << "[*]" << std::endl;
+    sleep(1);
+    std::cout << "^J^" << std::endl;
+    sleep(1);
+    std::cout << "o~~" << std::endl;
+    sleep(1);
+    std::cout << "Next, the computer will draw one of the symbols randomly."
+              << std::endl;
+    sleep(2);
+    std::cout << "If you chose the same symbol you'll get double of the money "
+                 "you bet."
+              << std::endl;
+    std::cout << "In other case you'll lose the money you bet." << std::endl;
+    sleep(2);
+    std::cout << "Pretty simple, right? Let us begin!" << std::endl;
+    sleep(2);
+  }
+  void errorMessage() {
+    std::cout << "Invalid input. Program will be terminated.\n";
+    exit(EXIT_FAILURE);
+  }
+};
 
-    void loadTheSymbol()//Loads the random symbol from the file
-    {
-      srand(time(NULL));
-      std::string line;
-      int lineNumber = 1;
-      int randomNumber = (std::rand() % 5) + 1;
-      std::fstream symbolsFile;
-      symbolsFile.open("symbols.txt");
-      if (!symbolsFile.good())
-      {
-        std::cout << "Could not open the file. Program will be terminated. Check if the file" << std::endl << "\"symbols.txt\" is in the folder.\n";
-        exit(EXIT_FAILURE);
-      }
-      while(getline(symbolsFile,line))
-      {
-         if (lineNumber == randomNumber)
-         {
-            chosenSymbol = line;
-            break;
-         }
-        lineNumber++;
-      }
-    symbolsFile.close();
+class guessingGame {
+ private:
+  std::string chosenSymbol;
+  std::string userInput;
+  double money;
+  double bet;
+
+ public:
+  double getMoney() { return money; }
+  void setMoney(double _money) { money = _money; }
+  void loadSymbol()  // Loads the random symbol from the file
+  {
+    srand(time(NULL));
+    std::string line;
+    int lineNumber = 1;
+    int randomNumber = (std::rand() % 5) + 1;
+    std::fstream symbolsFile;
+    symbolsFile.open("symbols.txt");
+    if (!symbolsFile.good()) {
+      std::cout << "Could not open the file. Program will be terminated. Check "
+                   "if the file"
+                << std::endl
+                << "\"symbols.txt\" is in the folder.\n";
+      exit(EXIT_FAILURE);
     }
-    void receiveInput()//Prompts user to enter his guess and the amount of many he has/wants to bet
-    {
+    while (getline(symbolsFile, line)) {
+      if (lineNumber == randomNumber) {
+        chosenSymbol = line;
+        break;
+      }
+      lineNumber++;
+    }
+    symbolsFile.close();
+  }
+  void receiveInput()  // Prompts user to enter his guess and the amount of many
+                       // he has/wants to bet
+  {
+    Messages message;
     std::cout << std::endl;
     std::cout << "How much money do you want to bet?" << std::endl;
     std::cin >> bet;
-    if (std::cin.fail())
-    {
-      std::cout << "Invalid input. Program will be terminated.\n";
-      exit (EXIT_FAILURE);
+    if (std::cin.fail()) {
+      message.errorMessage();
     }
-    while (bet < 0)
-    {
-      std::cout << "You can't bet less money than zero! Try again: " << std::endl;
+    while (bet < 0) {
+      std::cout << "You can't bet less money than zero! Try again: "
+                << std::endl;
       std::cin >> bet;
-      if (std::cin.fail())
-       {
-        std::cout << "Invalid input. Program will be terminated.\n";
-        exit (EXIT_FAILURE);
-       }
+      if (std::cin.fail()) {
+        message.errorMessage();
+      }
     }
-     while (bet > money)
-    {
-       std::cout << "You can't bet more money than you have! Try again: " << std::endl;
-       std::cin >> bet;
-       if (std::cin.fail())
-       {
-        std::cout << "Invalid input. Program will be terminated.\n";
-        exit (EXIT_FAILURE);
-       }
+    while (bet > money) {
+      std::cout << "You can't bet more money than you have! Try again: "
+                << std::endl;
+      std::cin >> bet;
+      if (std::cin.fail()) {
+        message.errorMessage();
+      }
     }
     std::cout << "Choose the symbol: ";
     std::cin.clear();
-    std::cin.ignore(1000,'\n');
+    std::cin.ignore(1000, '\n');
     getline(std::cin, userInput);
-    while (userInput != "-.-" && userInput != "@.@" && userInput != "[*]" && userInput != "^J^" && userInput != "o~~")
-    {
-        std::cout << "You chose non existent symbol. Try again." << std::endl;
-        std::cout << "Choose the symbol: ";
-        getline (std::cin, userInput);
+    while (userInput != "-.-" && userInput != "@.@" && userInput != "[*]" &&
+           userInput != "^J^" && userInput != "o~~") {
+      std::cout << "You chose non existent symbol. Try again." << std::endl;
+      std::cout << "Choose the symbol: ";
+      getline(std::cin, userInput);
     }
+  }
+  void setResult()  // Shows user the result and the amount of money he has now
+  {
+    if (chosenSymbol == userInput) {
+      money += 2 * bet;
+      std::cout << "You guessed right! You now have " << money << " money."
+                << std::endl;
+    } else {
+      money -= bet;
+      std::cout << "You guessed wrong! You now have " << money << " money."
+                << std::endl;
+      std::cout << "The correct symbol was " << chosenSymbol << "."
+                << std::endl;
     }
-    void showResult()//Shows user the result and the amount of money he has now
-    {
-        if (chosenSymbol == userInput)
-        {
-            money += 2*bet;
-            std::cout << "You guessed right! You now have " << money << " money." << std::endl;
-        }
-        else
-        {
-            money -= bet;
-            std::cout << "You guessed wrong! You now have " << money << " money." << std::endl;
-            std::cout << "The correct symbol was " << chosenSymbol << "." << std::endl;
-        }
-    }
-    void menu()//Shows the menu
-    {
-
-    std::cout << std::endl;
-    std::cout << "0) Exit" << std::endl
-              << "1) Play" << std::endl;
-    std::cout << std::endl;
-    }
+  }
+  void showResult() {
+    std::cout << "Thank you for playing!";
+    if (this->getMoney() >= 0)
+      std::cout << " You ended up with " << this->getMoney() << " money.\n";
+    else
+      std::cout
+          << "You ended up with " << abs(this->getMoney())
+          << " debt.\n";  // A feature can be started by making it possible
+                          // for user to bet more money then they have
+  }
 };
-int main()
-{
- std::cout << "Welcome to the Guessing Game!" << std::endl;
- sleep(2);
- std::cout << "You'll bet money on one of the following symbols: " << std::endl;
- sleep(2);
- std::cout << "-.-" << std::endl;
- sleep(1);
- std::cout << "@.@" << std::endl;
- sleep(1);
- std::cout << "[*]" << std::endl;
- sleep(1);
- std::cout << "^J^" << std::endl;
- sleep(1);
- std::cout << "o~~" << std::endl;
- sleep(1);
- std::cout << "Next, the computer will draw one of the symbols randomly." << std::endl;
- sleep(2);
- std::cout << "If you chose the same symbol you'll get double of the money you bet." << std::endl;
- std::cout << "In other case you'll lose the money you bet." << std::endl;
- sleep(2);
- std::cout << "Pretty simple, right? Let us begin!" << std::endl;
- sleep(2);
- guessingGame game;
- int choice = 0;
- std::cout << "How much money do you have?" << std::endl;
- std::cin >> game.money;
- if (std::cin.fail())
- {
-  std::cout << "Invalid input. Program will be terminated.\n";
-  exit (EXIT_FAILURE);
- }
-while (game.money <= 0)
-{
-  std::cout << "You must have more money than 0 to play. Try again: " << std::endl;
-  std::cin >> game.money;
-  if (std::cin.fail())
-    {
-      std::cout << "Invalid input. Program will be terminated.\n";
-      exit (EXIT_FAILURE);
+
+int main() {
+  Messages message;
+  guessingGame game;
+  message.beginMessage();
+  int choice = 0;
+  double _money = 0;
+  do {
+    std::cout << "How much money do you have?" << std::endl;
+    std::cin >> _money;
+    if (std::cin.fail()) {
+      message.errorMessage();
     }
-}
-    game.menu();
-    std::cin >> choice;
- if (std::cin.fail())
-    {
-       std::cout << "Invalid input. Program will be terminated.\n";
-       exit (EXIT_FAILURE);
+    if (_money <= 0) {
+      std::cout << "You must have more money than 0 to play. Try again: "
+                << std::endl;
     }
- while (choice != 0 && choice != 1)
- {
+  } while (_money <= 0);
+  game.setMoney(_money);
+  message.menu();
+  std::cin >> choice;
+  if (std::cin.fail()) {
+    message.errorMessage();
+  }
+  while (choice != 0 && choice != 1) {
     std::cout << "No such choice in the menu. Try again." << std::endl;
     std::cin >> choice;
-    if (std::cin.fail())
-     {
-       std::cout << "Invalid input. Program will be terminated.";
-       exit (EXIT_FAILURE);
-     }
- }
- while (choice == 1)
-  {
-    game.loadTheSymbol();
-    game.receiveInput();
-    game.showResult();
-    if (game.money <= 0) break;
-    game.menu();
-    std::cin >> choice;
-    if (std::cin.fail())
-    {
-       std::cout << "Invalid input. Program will be terminated.\n";
-       exit (EXIT_FAILURE);
+    if (std::cin.fail()) {
+      message.errorMessage();
     }
-    while (choice != 0 && choice != 1)
-    {
+  }
+  while (choice == 1) {
+    game.loadSymbol();
+    game.receiveInput();
+    game.setResult();
+    if (game.getMoney() <= 0) break;
+    message.menu();
+    std::cin >> choice;
+    if (std::cin.fail()) {
+      message.errorMessage();
+    }
+    while (choice != 0 && choice != 1) {
       std::cout << "No such choice in the menu. Try again." << std::endl;
       std::cin >> choice;
-      if (std::cin.fail())
-      {
-         std::cout << "Invalid input. Program will be terminated.\n";
-         exit (EXIT_FAILURE);
+      if (std::cin.fail()) {
+        message.errorMessage();
       }
     }
- }
- sleep (2);
- std::cout << std::endl;
- std::cout << "Thank you for playing!";
- if (game.money >= 0) std::cout << " You ended up with " << game.money << " money.\n";
- else std::cout << "You ended up with " << abs(game.money) << " debt.\n"; //A feature can be started by making it possible for user to bet more money then they have
- return 0;
+  }
+  game.showResult();
+  return 0;
 }
-
-
